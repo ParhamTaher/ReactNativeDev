f50a6c1ff1075ad187137862d768fb00a0a5965a
Parham Taher <parham.taher@gmail.com>
2017-08-09 21:20:07 -0400

    Completed section 15 + readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ReactNativeDev
+
+My Journey through learning React Native along side regular react in order to develop on web and mobile platforms.
--- manager/src/App.js
+++ manager/src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyBPGELyyxLrSO0AjAxlndswb-j-d5RZHeg',
+            authDomain: 'manager-edaf5.firebaseapp.com',
+            databaseURL: 'https://manager-edaf5.firebaseio.com',
+            projectId: 'manager-edaf5',
+            storageBucket: 'manager-edaf5.appspot.com',
+            messagingSenderId: '954461699956'
+        };
+
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>Hello World!</Text>
-                </View>
+                <LoginForm />
             </Provider>
         );
     }
